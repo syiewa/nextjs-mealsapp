8ca94b7206646d2e115e7397f2f468ed02399bca
syiewa <wasi.arnosa@playonsports.com>
2025-01-05 17:52:22 +0700

    change access key to acces key id

--- src/lib/meals.tsx
+++ src/lib/meals.tsx
@@ -8,7 +8,7 @@ import { createClient } from '@/utils/supabase/client';
 
 const s3 = new S3({
   credentials: {
-    accessKeyId: process.env.AWS_ACCESS_KEY || '',
+    accessKeyId: process.env.AWS_ACCESS_KEY_ID || '',
     secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY || '',
   },
   region: process.env.AWS_REGION || '',
